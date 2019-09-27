42b023384af0998f915285304517a601afc59c5f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 15:32:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,8 +12,8 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
   end
 
   def self.all
