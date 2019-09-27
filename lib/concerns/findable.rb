6bf659bf16e9ddfbf8a90a58c3d992583a5ff488
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 15:43:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,5 @@
 module Findable
 
-  def find_by_name(name)
-    self.all.detect {|a| a.name == name  }
-
-  end
-end
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
+  #end
