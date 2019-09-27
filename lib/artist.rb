51bff0d6fa100a455f915b7078516450afcb3684
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 15:31:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  extend Memorable 
+  extend Memorable
   attr_accessor :name
   attr_reader :songs
 
@@ -24,9 +24,7 @@ class Artist
   #  self.all.clear
   #end
 
-  #def self.count
-  #  self.all.count
-  #end
+  
 
   def add_song(song)
     @songs << song
